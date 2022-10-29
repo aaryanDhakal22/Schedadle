550731fdd8837d0bd33ad668e069f46e884cb98d
aaryanDhakal22 <aaryan.dhakal22@gmail.com>
2022-10-29 14:20:15 -0400

    Added html to list conversion

--- htmltodict.py
+++ htmltodict.py
@@ -1,8 +1,18 @@
 from os import listdir
 from bs4 import BeautifulSoup
+from pprint import pprint
 
 files = listdir("./schedules")
 for file in files:
     contents = open("./schedules/" + file, "r").read()
     soup = BeautifulSoup(contents, "html.parser")
-    print(soup.prettify())
+    all_rows = soup.find_all("tr")[3:]
+    rows_info = []
+
+    for i in all_rows:
+        all_info = i.text.split("\n")
+        all_info = [i for i in all_info if i != ""]
+        print(len(all_info))
+        rows_info.append(all_info)
+
+    # pprint(rows_info)
